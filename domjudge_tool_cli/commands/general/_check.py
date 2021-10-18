a34ed664e9852ccb94534956583102b581907513
Jason Xie <x5758x@gmail.com>
2021-10-18 21:52:20 +0800

    fix(general): fix config dom server and install

--- domjudge_tool_cli/commands/general/_check.py
+++ domjudge_tool_cli/commands/general/_check.py
@@ -22,22 +22,24 @@ def create_config(
     host: str,
     username: str,
     password: str,
-    disable_ssl: bool = False,
+    disable_ssl: bool = typer.Option(False),
     timeout: Optional[float] = None,
     max_connections: Optional[int] = None,
     max_keepalive_connections: Optional[int] = None,
 ) -> DomServerClient:
+    typer.echo("*" * len(password))
     dom_server = DomServerClient(
         host=host,
         username=username,
         password=password,
-        disable_ssl=disable_ssl,
+        disable_ssl=disable_ssl or False,
         timeout=timeout,
         max_connections=max_connections,
         max_keepalive_connections=max_keepalive_connections,
     )
     with open("domserver.json", "wb+") as f:
         f.write(dom_server.json().encode())
+        typer.echo("Success config Dom Server.")
 
     return dom_server
 
